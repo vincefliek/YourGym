7a12d27a710f7f1596a55c3a105d00e08409786d
vince fliek <13233155+vincefliek@users.noreply.github.com>
2022-02-20 01:28:40 +0300

    server: update server messages

--- server/index.js
+++ server/index.js
@@ -3,9 +3,9 @@ const app = express();
 const port = 3100;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('YourGym server welcomes you!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`YourGym server is listening on port ${port}`);
 });
