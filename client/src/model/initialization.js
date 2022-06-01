703dee3f7fd2bcfb8f128edf52461d9993739d48
vince fliek <13233155+vincefliek@users.noreply.github.com>
2022-06-01 23:54:10 +0200

    init firebase

--- client/src/model/initialization.js
+++ client/src/model/initialization.js
@@ -1,8 +1,24 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
 import { Store } from './store';
 import { createAPIs } from './apis';
 import { Validator } from './validation';
 
 export const initApp = () => {
+  const firebaseConfig = {
+    apiKey: 'AIzaSyBm80icd-F0c8tJZO1uYlKMlLa4Coau81A',
+    authDomain: 'your-gym-b564f.firebaseapp.com',
+    databaseURL:
+      'https://your-gym-b564f-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'your-gym-b564f',
+    storageBucket: 'your-gym-b564f.appspot.com',
+    messagingSenderId: '933355137079',
+    appId: '1:933355137079:web:515ec1551eb8ab84c677b2',
+  };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
   const store = new Store();
   const validator = new Validator();
 
