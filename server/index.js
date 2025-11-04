36c5e0a827ac6c9e5ae99a586ccbf42541a92e14
Vince Fliek <13233155+vincefliek@users.noreply.github.com>
2025-11-04 23:58:33 +0100

    test

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3100;
 
+// process.env.DB_URL
+// process.env.DB_KEY
+
 app.get('/', (req, res) => {
   res.send('YourGym server welcomes you!');
 });
