579dee96ea6235f500c4bc9c63ac423928428720
Vince Fliek <13233155+vincefliek@users.noreply.github.com>
2026-01-01 12:03:37 +0100

    client: implement test fullStartFinish

---
+++ client/src/test-user-stories/inProgressTraining/fullStartFinish.spec.tsx
@@ -0,0 +1,76 @@
+import { act } from 'react';
+import userEvent from '@testing-library/user-event';
+import { TestDriver, createDriver } from '../../test-utils';
+
+describe('in progress training', () => {
+  let driver: TestDriver;
+
+  beforeEach(async () => {
+    driver = createDriver();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test('start template training and finish with completed training on the home screen', async () => {
+    await driver.render.app();
+
+    // back to home screen
+    expect(await driver.waitFor.byTestId('home-screen')).toBeInTheDocument();
+
+    // check there are no completed trainings yet
+    await expect(
+      driver.waitFor.byTestId('completed-training-item'),
+    ).rejects.toThrow();
+
+    await driver.create.firstTemplateTraining();
+
+    // open the newly created training
+    const trainingBox = await driver.waitFor.byText(/New Training/i);
+    act(() => userEvent.click(trainingBox));
+
+    // click start
+    const startTrainingButton = await driver.waitFor.byTestId(
+      'start-training-button',
+    );
+    act(() => userEvent.click(startTrainingButton));
+
+    // inside of create exercise screen
+    expect(
+      await driver.waitFor.byTestId('existing-exercise-screen'),
+    ).toBeInTheDocument();
+
+    // mark set as completed
+    const completeSetBtn = await driver.waitFor.byTestId('done-set-button');
+    act(() => userEvent.click(completeSetBtn));
+
+    // go to previous screen
+    const backBtn = await driver.waitFor.byTestId('back-button');
+    act(() => userEvent.click(backBtn));
+
+    // inside of create training screen
+    expect(
+      await driver.waitFor.byTestId('existing-training-screen'),
+    ).toBeInTheDocument();
+
+    // finish training
+    const finishTrainingBtn = await driver.waitFor.byTestId(
+      'finish-training-button',
+    );
+    act(() => userEvent.click(finishTrainingBtn));
+
+    // back to home screen
+    expect(await driver.waitFor.byTestId('home-screen')).toBeInTheDocument();
+
+    // check that completed training is added
+    const completedTrainingsAfter = await driver.waitFor.allByTestId(
+      'completed-training-item',
+    );
+    expect(completedTrainingsAfter.length).toBe(1);
+
+    // check that completed training is shown on the home screen
+    expect(await driver.waitFor.byText(/New Training/i)).toBeInTheDocument();
+  });
+});
