d6c50d45d1fd55a92db8fedf4c83bff18c92a46d
Vince Fliek <13233155+vincefliek@users.noreply.github.com>
2026-01-04 19:59:39 +0100

    client: implement test delete.exercise.spec

--- client/src/test-user-stories/existingTraining/delete.exercise.spec.tsx
+++ client/src/test-user-stories/existingTraining/delete.exercise.spec.tsx
@@ -1,5 +1,63 @@
-test('TDB', () => {
-  expect(true).toBe(true);
-});
+import { act } from 'react';
+import userEventBuilder from '@testing-library/user-event';
 
-export {};
+import { createDriver, type TestDriver } from '../../test-utils';
+import { waitFor } from '@testing-library/react';
+
+describe('existing training', () => {
+  let driver: TestDriver;
+
+  beforeEach(async () => {
+    driver = createDriver();
+  });
+
+  afterEach(() => {
+    jest.resetAllMocks();
+    jest.clearAllMocks();
+  });
+
+  test('delete an exercise from an existing training', async () => {
+    const userEvent = userEventBuilder.setup();
+
+    await driver.render.app();
+
+    // create a training with two exercises
+    await driver.create.firstTemplateTraining({
+      exercises: [
+        { sets: [{ reps: 8, weight: 12 }] },
+        { sets: [{ reps: 6, weight: 10 }] },
+      ],
+    });
+
+    // open the newly created training
+    const trainingBox = await driver.waitFor.byText(/New Training/i);
+    await act(() => userEvent.click(trainingBox));
+
+    // click edit the newly created training
+    const editTrainingIcon = await driver.waitFor.byTestId(
+      'training-edit-button',
+    );
+    await act(() => userEvent.click(editTrainingIcon));
+
+    // inside of edit existing training screen
+    expect(
+      await driver.waitFor.byTestId('edit-existing-training-screen'),
+    ).toBeInTheDocument();
+
+    // ensure two exercises present
+    const itemsBefore = await driver.waitFor.allByTestId('exercise-item');
+    expect(itemsBefore.length).toBe(2);
+
+    // click delete on the first exercise
+    const deleteButtons = await driver.waitFor.allByTestId(
+      'delete-exercise-button',
+    );
+    await act(() => userEvent.click(deleteButtons[0]));
+
+    await waitFor(async () => {
+      // expect one exercise remaining
+      const itemsAfter = await driver.waitFor.allByTestId('exercise-item');
+      expect(itemsAfter.length).toBe(1);
+    });
+  });
+});
